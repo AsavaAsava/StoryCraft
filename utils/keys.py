dce04b9b79bd4843b00b9d1c0366ecaa95fe3370
Nathan Mbugua <78223048+some-casual-coder@users.noreply.github.com>
2023-11-11 03:32:21 +0300

    Delete utils/keys.py
    fix keys error

--- utils/keys.py
+++
@@ -1,4 +0,0 @@
-eleven_key = "55ab95c3d98b6dbbd7f610ed5ae89e45"
-OPENAI_API_KEY = "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"
-REPLICATE_API_TOKEN = "r8_cD2iYmXt4aoBkPGFS3e0sweOQYVm9UH48yOrZ"
-GOOGLE_APPLICATION_CREDENTIALS = r"C:\Users\Nathan\Downloads\service-creds.json"
